edd9291db59a9e650851660034ffe7820e8eb548
NotCapengeR <purple.ka4ok@gmail.com>
2022-03-12 02:31:25 +0500

    Homework 1.3.2 have done!

--- src/main/kotlin/transfers/main.kt
+++ src/main/kotlin/transfers/main.kt
@@ -7,19 +7,45 @@ import java.text.DecimalFormat
 private const val RED_TXT = "\u001B[31m"
 private const val RESET_TXT = "\u001B[0m"
 private val decimalFormat = DecimalFormat("###.##")
+private val transferSystems = listOf(
+    TransferSystem.Mastercard,
+    TransferSystem.Maestro,
+    TransferSystem.Visa,
+    TransferSystem.MIR,
+    TransferSystem.VKPay
+)
+
+enum class TransferSystem {
+    Mastercard,
+    Maestro,
+    Visa,
+    MIR,
+    VKPay
+}
 
 fun main() {
-    println("Enter negative or zero number to finish the program.")
+    val transferSumInMonth = mutableListOf<BigDecimal>()
+    repeat(transferSystems.size) { // Для каждой системы свой счётчик месячных лимитов.
+        transferSumInMonth.add(BigDecimal(0))
+    }
+    println("Enter zero number to finish the program.")
     while (true) {
         try {
-            println("${RESET_TXT}Enter the amount for transfer (in ₽):${RESET_TXT}")
-            val transferred = BigDecimal(readLine()!!.trim().toDouble()).setScale(2, RoundingMode.HALF_DOWN)
-            if (transferred <= BigDecimal(0)) break
-            if (transferred <= BigDecimal(35)) {
-                println("${RED_TXT}Not enough funds to transfer money!${RED_TXT}")
+            println("${RESET_TXT}Choose your transfer system:\n1) Mastercard\n2) Maestro\n3) Visa\n4) MIR\n5) VK Pay${RESET_TXT}")
+            val inputTransferSystem: Int = readLine()!!.trim().toInt()
+            if (inputTransferSystem == 0) break
+            if (inputTransferSystem < 0 || inputTransferSystem > transferSystems.size) {
+                println("${RED_TXT}Error: Transfer system you entered does not exist!${RED_TXT}")
                 continue
             }
-            val commission: BigDecimal = commission(transferred)
+            val transferSystem: TransferSystem = transferSystems[inputTransferSystem - 1]
+            println("Enter the amount for transfer (in ₽)")
+            val transferred = BigDecimal(readLine()!!.trim().toDouble()).setScale(2, RoundingMode.HALF_DOWN)
+            if (!checkLimits(transferred, transferSumInMonth[inputTransferSystem - 1], transferSystem)) continue
+
+            val commission: BigDecimal =
+                commission(transferred, transferSumInMonth[inputTransferSystem - 1], transferSystem)
+            transferSumInMonth[inputTransferSystem - 1] += totalTransferred(transferred, commission)
             println(
                 "Input amount for transfer: ${decimalFormat.format(transferred)} ₽.\n" +
                         "Commission: ${decimalFormat.format(commission)} ₽.\n" +
@@ -32,10 +58,41 @@ fun main() {
     println("Finishing the program...")
 }
 
-private fun commission(amount: BigDecimal): BigDecimal {
-    val commission: BigDecimal = amount * BigDecimal(0.0075)
-    return if (commission > BigDecimal(35)) commission
-    else BigDecimal(35)
+private fun commission(amount: BigDecimal, transferSumInMonth: BigDecimal, transferSystem: TransferSystem): BigDecimal {
+    return when (transferSystem) {
+        TransferSystem.VKPay -> BigDecimal(0)
+        TransferSystem.Visa, TransferSystem.MIR -> {
+            val commission: BigDecimal = amount * BigDecimal(0.0075)
+            if (commission > BigDecimal(35)) commission
+            else BigDecimal(35)
+        }
+        TransferSystem.Maestro, TransferSystem.Mastercard -> {
+            if (transferSumInMonth + amount <= BigDecimal(75000)) BigDecimal(0)
+            else amount * BigDecimal(0.006) + BigDecimal(20)
+        }
+    }
+}
+
+private fun checkLimits(amount: BigDecimal, transferSumInMonth: BigDecimal, transferSystem: TransferSystem): Boolean {
+    if (amount <= BigDecimal(35) && (transferSystem == TransferSystem.MIR || transferSystem == TransferSystem.Visa)) {
+        println("${RED_TXT}Not enough funds to transfer money (you need 35 ₽ to pay commission)!${RED_TXT}")
+        return false
+    }
+
+    if (transferSystem == TransferSystem.VKPay &&
+        (amount > BigDecimal(15_000) || transferSumInMonth + amount > BigDecimal(40_000))
+    ) {
+        println("${RED_TXT}You have exceeded the allowable limits for this month or this transaction!${RED_TXT}")
+        return false
+    }
+    if (transferSystem != TransferSystem.VKPay &&
+        (amount > BigDecimal(150_000) || transferSumInMonth + amount > BigDecimal(600_000))
+    ) {
+        // Здесь сутки я посчитал за один раз
+        println("${RED_TXT}You have exceeded the allowable limits for this month or this transaction!${RED_TXT}")
+        return false
+    }
+    return true
 }
 
 private fun totalTransferred(transferred: BigDecimal, commission: BigDecimal): BigDecimal {
