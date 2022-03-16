fa16023a45fbe3272e6dfb860ebf2ed66b245c77
NotCapengeR <purple.ka4ok@gmail.com>
2022-03-16 22:35:00 +0500

    Did with LocalDateTime

--- src/main/kotlin/online/main.kt
+++ src/main/kotlin/online/main.kt
@@ -1,42 +1,37 @@
 package online
 
-import java.text.DateFormatSymbols
-import java.text.SimpleDateFormat
-import java.util.*
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.time.temporal.ChronoUnit
 
-
-data class User(val name: String, val gender: Gender, var lastSeen: Long)
+data class User(val name: String, val gender: Gender, var lastSeen: LocalDateTime)
 
 enum class Gender {
     MALE,
     FEMALE
 }
 
-private val monthsAndYearsFormat = SimpleDateFormat("d MMMM yyyy в H:mm")
-private val todayFormat = SimpleDateFormat("в H:mm")
-private val weekFormatSymbols = object : DateFormatSymbols() {
-    override fun getWeekdays(): Array<String> = arrayOf(
-        "в понедельник", "во вторник", "в среду", "в четверг", "в пятницу", "в субботу", "в воскресенье"
-    )
-}
-private val weekFormat = SimpleDateFormat("EEEE в H:mm", weekFormatSymbols)
+private val monthsAndYearsFormat = DateTimeFormatter.ofPattern("d MMMM yyyy в H:mm")
+private val todayFormat = DateTimeFormatter.ofPattern("в H:mm")
+private val absFormat = DateTimeFormatter.ofPattern("dd-MM-yyyy, HH:mm:ss")
+private val weekFormat = DateTimeFormatter.ofPattern("EEEE в H:mm")
 
 fun main() {
     val users = mutableListOf<User>()
-    val smile = User("ヅ", Gender.MALE, 300)
-    smile.lastSeen = 3000
-    users.add(0, User("Валера", Gender.MALE, 2_119_100))
-    users.add(1, User("Alex", Gender.MALE, 30))
-    users.add(2, User("Вася", Gender.MALE, 36_001))
-    users.add(3, User("Олег", Gender.MALE, 10002))
-    users.add(4, User("Женя :)", Gender.MALE, 527200))
-    users.add(5, User("Аркадий", Gender.MALE, 21_030_400))
-    users.add(6, User("Crazy Frog", Gender.MALE, 172_800))
-    users.add(7, User("Дональд Трамп", Gender.MALE, 29_030_401))
+    val smile = User("ヅ", Gender.MALE, LocalDateTime.parse("16-02-2022, 09:00:00", absFormat))
+    smile.lastSeen = LocalDateTime.parse("07-08-2021, 09:00:00", absFormat)
+    users.add(0, User("Валера", Gender.MALE, LocalDateTime.parse("16-03-2022, 09:00:00", absFormat)))
+    users.add(1, User("Alex", Gender.MALE, LocalDateTime.parse("15-03-2022, 09:00:00", absFormat)))
+    users.add(2, User("Вася", Gender.MALE, LocalDateTime.parse("15-03-2020, 09:00:00", absFormat)))
+    users.add(3, User("Олег", Gender.MALE, LocalDateTime.parse("16-03-2022, 21:54:01", absFormat)))
+    users.add(4, User("Женя :)", Gender.MALE, LocalDateTime.parse("07-08-2021, 09:00:00", absFormat)))
+    users.add(5, User("Аркадий", Gender.MALE, LocalDateTime.parse("07-08-2021, 09:00:00", absFormat)))
+    users.add(6, User("Crazy Frog", Gender.MALE, LocalDateTime.parse("07-08-2021, 09:00:00", absFormat)))
+    users.add(7, User("Дональд Трамп", Gender.MALE, LocalDateTime.parse("07-08-2021, 09:00:00", absFormat)))
     users.add(8, smile)
-    users.add(9, User("Пугачёва", Gender.FEMALE, 21_030_401))
-    users.add(10, User("Ольга Бузова", Gender.FEMALE, 0))
-    users[1].lastSeen = 120
+    users.add(9, User("Пугачёва", Gender.FEMALE, LocalDateTime.parse("12-03-2022, 09:30:00", absFormat)))
+    users.add(10, User("Ольга Бузова", Gender.FEMALE, LocalDateTime.parse("16-03-2022, 21:30:00", absFormat)))
+    users[1].lastSeen = LocalDateTime.parse("15-03-2022, 03:00:00", absFormat)
     users.forEach {
         println("${it.name} ${if (it.gender == Gender.MALE) "был" else "была"} ${agoToText(it.lastSeen)}")
     }
@@ -52,38 +47,47 @@ private fun caseFormat(number: String, one: String, fromTwoToFour: String, other
     }
 }
 
-private fun agoToText(lastSeen: Long): String {
-    val currentDate = Date()
-    val lastSeenToDate = Date(currentDate.time - (lastSeen * 1000))
-    if (lastSeen >= 0) {
-        return when (lastSeen) {
+private fun agoToText(lastSeen: LocalDateTime): String {
+    val currentDate = LocalDateTime.now()
+    val lastSeenToSeconds = ChronoUnit.SECONDS.between(lastSeen, currentDate)
+    if (lastSeenToSeconds >= 0) {
+        return when (lastSeenToSeconds) {
             in 0..60 -> "только что"
             in 61..3600 -> "в сети ${
                 caseFormat(
-                    (lastSeen / 60).toString(),
+                    (lastSeenToSeconds / 60).toString(),
                     "минуту",
-                    "${(lastSeen / 60)} минуты",
-                    "${(lastSeen / 60)} минут"
+                    "${(lastSeenToSeconds / 60)} минуты",
+                    "${(lastSeenToSeconds / 60)} минут"
                 )
             } назад"
             in 3601..36_000 -> "в сети ${
                 caseFormat(
-                    (lastSeen / 3600).toString(),
+                    (lastSeenToSeconds / 3600).toString(),
                     "час",
-                    "${(lastSeen / 3600)} часа",
-                    "${(lastSeen / 3600)} часов"
+                    "${(lastSeenToSeconds / 3600)} часа",
+                    "${(lastSeenToSeconds / 3600)} часов"
                 )
             } назад"
-            in 36_001..86_400 -> "в сети ${if (lastSeenToDate.day != currentDate.day) "вчера" else "сегодня"}" +
-                    " ${todayFormat.format(lastSeenToDate)}"
+            in 36_001..86_400 -> "в сети ${if (lastSeen.dayOfYear != currentDate.dayOfYear) "вчера" else "сегодня"}" +
+                    " ${todayFormat.format(lastSeen)}"
             in 86_401..604_800 -> "в сети ${
-                when (lastSeen / 86_400) {
-                    1L -> "вчера ${todayFormat.format(lastSeenToDate)}"
-                    2L -> "позавчера ${todayFormat.format(lastSeenToDate)}"
-                    else -> weekFormat.format(lastSeenToDate)
+                when (lastSeenToSeconds / 86_400) {
+                    1L -> "вчера ${todayFormat.format(lastSeen)}"
+                    2L -> "позавчера ${todayFormat.format(lastSeen)}"
+                    else -> accusativeDay(weekFormat.format(lastSeen))
                 }
             }"
-            else -> "в сети ${monthsAndYearsFormat.format(lastSeenToDate)}"
+            else -> "в сети ${monthsAndYearsFormat.format(lastSeen)}"
         }
-    } else throw IllegalArgumentException("Error: Time could not be negative!")
+    } else throw IllegalArgumentException("Error: Invalid date (this is not a Back to Future)!")
 }
+
+private fun accusativeDay(day: String): String = day
+    .replace("понедельник", "в понедельник")
+    .replace("вторник", "во вторник")
+    .replace("среда", "в среду")
+    .replace("четверг", "в четверг")
+    .replace("пятница", "в пятницу")
+    .replace("суббота", "в субботу")
+    .replace("воскресенье", "в воскресенье")
