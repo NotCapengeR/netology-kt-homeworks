20f4c44a2e7338b02d5394f47cb300bf20e9a119
NotCapengeR <purple.ka4ok@gmail.com>
2022-03-23 02:07:17 +0500

    Homework 2.2 have done!

--- src/main/kotlin/wall/main.kt
+++ src/main/kotlin/wall/main.kt
@@ -13,21 +13,25 @@ fun main() {
     WallService.addPost("hueta1", valera, valera)
     WallService.addPost("hueta2", valera, valera)
     WallService.outputUserWall(valera)
-    WallService.updatePost(1, "hueta4")
+    WallService.updatePost(1, "hueta4", valera)
+    println()
+    WallService.outputUserWall(valera)
+    println()
+    WallService.deletePost(2)
     WallService.outputUserWall(valera)
 }
 
 object WallService {
     private val posts = HashMap<Long, MutableList<Post>>()
-    private var id = 0L
+    private var id = 1L
 
     fun outputUserWall(user: User) {
         posts[user.id]?.forEach {
-            println(it)
-        }
+            println(it) // Лень было toString() переопределять для красивого вывода)
+        }               // Энивей для нормального отображения будет UI использоваться
     }
 
-    fun addPost(postText: String, wallOwner: User, author: User) {
+    fun addPost(postText: String, wallOwner: User, author: User): Boolean { //Always true
         val post = Post(
             id = id,
             wallOwnerId = wallOwner.id,
@@ -44,18 +48,92 @@ object WallService {
         currentUserPosts.add(post)
         posts[wallOwner.id] = currentUserPosts
         id++
+        return true
     }
 
-    fun updatePost(postId: Long, newText: String) {
+    fun updatePost(postId: Long, newText: String, updateAuthor: User): Boolean {
         val post = findPostById(postId)
-        if (post != null) {
+        if (post != null && updateAuthor.id == post.authorId) {
             val newPost = post.copy(text = newText)
             val postsList = posts[post.wallOwnerId]
             postsList?.set(postsList.indexOf(post), newPost)
+            return posts[post.wallOwnerId]?.contains(newPost)!!
         }
+        return false
+    }
+
+    fun deletePost(postId: Long): Boolean {
+        val post = findPostById(postId)
+        if (post != null) {
+            val postsList = posts[post.wallOwnerId]
+            postsList?.remove(post)
+            return !postsList?.remove(post)!!
+        }
+        return false
     }
 
     private fun findPostById(postId: Long): Post? {
+        posts.forEach { it ->
+            it.value.forEach {
+                if (it.id == postId) return it
+            }
+        }
+        return null
+    }
+}
+
+class WallServiceForTests {
+    private val posts = HashMap<Long, MutableList<Post>>()
+    private var id = 1L
+
+    fun outputUserWall(user: User) {
+        posts[user.id]?.forEach {
+            println(it)
+        }
+    }
+
+    fun addPost(postText: String, wallOwner: User, author: User): Boolean { //Always true
+        val post = Post(
+            id = id,
+            wallOwnerId = wallOwner.id,
+            authorId = author.id,
+            text = postText
+        )
+        val currentUserPosts = mutableListOf<Post>()
+        if (posts.containsKey(wallOwner.id)) {
+            val previousUserPosts: List<Post>? = posts[wallOwner.id]
+            previousUserPosts?.forEach {
+                currentUserPosts.add(it)
+            }
+        }
+        currentUserPosts.add(post)
+        posts[wallOwner.id] = currentUserPosts
+        id++
+        return true
+    }
+
+    fun updatePost(postId: Long, newText: String, updateAuthor: User): Boolean {
+        val post = findPostById(postId)
+        if (post != null && updateAuthor.id == post.authorId) {
+            val newPost = post.copy(text = newText)
+            val postsList = posts[post.wallOwnerId]
+            postsList?.set(postsList.indexOf(post), newPost)
+            return posts[post.wallOwnerId]?.contains(newPost)!!
+        }
+        return false
+    }
+
+    fun deletePost(postId: Long): Boolean {
+        val post = findPostById(postId)
+        if (post != null) {
+            val postsList = posts[post.wallOwnerId]
+            postsList?.remove(post)
+            return !postsList?.remove(post)!!
+        }
+        return false
+    }
+
+    fun findPostById(postId: Long): Post? {
         posts.forEach { it ->
             it.value.forEach {
                 if (it.id == postId) return it
